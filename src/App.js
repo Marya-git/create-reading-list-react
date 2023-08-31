e6264204116e9d24a54734ba0c99cced283870e2
Marya-git <mf786@scarletmail.rutgers.edu>
2023-08-30 21:14:28 -0700

    add API request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+
 
 function App() {
   const [books, setBooks] = useState([]);
   const [id, setId] = useState(0);
 
-  const editBookById = (id, newTitle) => {
+  const fetchAllBooks = async () => {
+   const response = await axios.get('http://localhost:3001/books');
+   setBooks(response.data);
+  };
+// only run fetchAllBooks funciton at a very specific time when our components first started to render on screen
+  useEffect(() => {
+    fetchAllBooks();
+  },[]);
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {title: newTitle});
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data};
       }
-
       return book;
     });
 
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -26,11 +38,16 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
+  const createBook = async (title) => {
     setId(id+1);
-    //id:id =>id
-    const updatedBooks = [...books,{id,title}];
+    const response = await axios.post('http://localhost:3001/books', {
+      // //title:title =>title
+      title
+    });
+        
+    const updatedBooks = [...books,response.data];
     setBooks(updatedBooks);
+    
   };
 
   return (
